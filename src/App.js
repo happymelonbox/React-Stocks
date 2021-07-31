00fec0b3e6f84f4c2c6f2f377b426feb3446d3af
Justin Berger Howes <howes.j.j@gmail.com>
2021-07-31 17:18:24 +1000

    fetching stocks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 import Header from './components/Header'
 import MainContainer from './containers/MainContainer'
 
+const URL = 'http://localhost:3000/stocks'
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      stocks: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(URL)
+    .then(resp=>resp.json())
+    .then(data=>{
+      return this.setState({
+      stocks: data
+    },()=>{console.log(this.state.stocks)})})
+  }
   render() {
     return (
       <div>
